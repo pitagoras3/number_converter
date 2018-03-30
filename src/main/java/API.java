2d5e6c2d0e460cae66ab352dd746d57ed9d763df
pitagoras3 <szymon.marcinkiewicz@gmail.com>
2018-03-30 19:32:39 +0200

    Added slf4j logging to avoid errors and to enable logging at launch.

--- src/main/java/API.java
+++ src/main/java/API.java
@@ -1,8 +1,11 @@
+import org.apache.log4j.BasicConfigurator;
+
 import static spark.Spark.*;
 
 public class API {
 
     public static void main(String[] args){
+        BasicConfigurator.configure();
         get("/hello", (req, res) -> "Hello World");
 
     }
