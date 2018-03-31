0a569bf85fed09a69f55dcdfebf0027ce43cc0f6
pitagoras3 <szymon.marcinkiewicz@gmail.com>
2018-03-31 13:39:13 +0200

    Created unit test structure.

---
+++ src/test/java/APITest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class APITest {
+
+    @Test
+    public void testConvertToRomanRequest(){
+
+    }
+
+    @Test
+    public void testConvertToHexRequest(){
+
+    }
+}
---
+++ src/test/java/ConverterTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+public class ConverterTest {
+
+    @Test
+    public void testConvertMethod(){
+
+    }
+
+    @Test
+    public void testConvertToRoman(){
+
+    }
+
+    @Test
+    public void testConvertToHex(){
+
+    }
+
+    @Test
+    public void testCheckIfCanParseNumber(){
+
+    }
+
+    @Test
+    public void testCheckIfNumberIsPositive(){
+
+    }
+
+}
